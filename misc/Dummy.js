114061a629140b930271fd00c6b043b552a86407
yeop <woco11@naver.com>
2023-11-20 20:28:44 +0900

    Add dummy images

--- misc/Dummy.js
+++ misc/Dummy.js
@@ -1,9 +1,11 @@
-const moment = require('moment');
+// const moment = require('moment');
 
-const dishes = [
-    {id: 0, name: "차돌된장찌개", date: moment('2023-10-13'), image: null, price: 5500, allergen: null, available: true},
-    {id: 1, name: "남산왕돈까스", date: moment('2023-10-13'), image: null, price: 5000, allergen: null, available: true},
-    {id: 2, name: "나가사키라멘", date: moment('2023-10-13'), image: null, price: 5000, allergen: null, available: true},
-    {id: 3, name: "돈사태찜", date: moment('2023-10-13'), image: null, price: 5500, allergen: null, available: true},
-    {id: 4, name: "참치김치볶음밥*계란후라이", date: moment('2023-10-13'), image: null, price: 5500, allergen: null, available: true},
+const porkCutletImage = require('./img/Pork_Cutlet.webp');
+
+export const dishes = [
+    {id: 0, name: "차돌된장찌개", date: '2023-10-13', image: porkCutletImage, price: 5500, allergen: null, available: true},
+    {id: 1, name: "남산왕돈까스", date: '2023-10-13', image: null, price: 5000, allergen: null, available: true},
+    {id: 2, name: "나가사키라멘", date: '2023-10-13', image: null, price: 5000, allergen: null, available: true},
+    {id: 3, name: "돈사태찜", date: '2023-10-13', image: null, price: 5500, allergen: null, available: true},
+    {id: 4, name: "참치김치볶음밥*계란후라이", date: '2023-10-13', image: null, price: 5500, allergen: null, available: true},
 ];

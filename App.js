46dde73034047b50a6f3327de0ec46fd8755c0fc
yeop <woco11@naver.com>
2023-11-20 20:53:26 +0900

    Add stylings for main and MenuCard
    * Add dependencies 'react-native-shadow-2'
    
    Co-authored-by: Ryu jongheon <me@lapy.link>

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, FlatList, SafeAreaView } from "react-native";
+
+import MenuCard from "./components/MenuCard";
+import { dishes } from "./misc/Dummy";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.starttitle}>
+        <Text style={styles.titleText}>오늘의 식단</Text>
+        <Text>2023년xx월xx일</Text>
+        { /* TODO: 날짜 동적으로 바꾸기 */ }
+      </View>
+      {/* <SafeAreaView>
+        <FlatList />
+      </SafeAreaView> */}
+      <View style="menuCards">
+        <MenuCard dish={dishes[0]} />
+      </View>
     </View>
   );
 }
@@ -13,8 +26,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  starttitle: {
+    alignSelf: "flex-start", // 왼쪽 정렬
+    marginLeft: 10, // 좌측 여백
+  },
+  titleText: {
+    fontSize: 35, //Text 폰트크기 조절
+  },
+  menuCards: {
+    width: '95%',
+  }
 });
--- components/MenuCard.js
+++ components/MenuCard.js
@@ -1,11 +1,51 @@
-const styles = {
+import { StyleSheet, Text, View, Image }  from 'react-native';
+import { Shadow } from 'react-native-shadow-2';
 
+export default function MenuCard({dish}) {
+    return (
+        <Shadow
+            radius={10}
+            offset={[0, 5]}
+        >
+            <View style={styles.container}>
+                <View style={styles.imageArea}>
+                    <Image
+                        source={dish.image}
+                        style={styles.image}
+                        resizeMode='cover'
+                    />
+                </View>
+                <View style={styles.infoArea}>
+                    <Text>Hello, world!</Text>
+                    <Text style={styles.menuName}>{dish.name}</Text>
+                    <Text style={styles.price}>{dish.price}</Text>
+                </View>
+            </View>
+        </Shadow>
+            
+    );
 };
 
-export default function MenuCard({id, name, date, image, price, allergen, available}) {
-    return (
-        <>
-
-        </>
-    );
-}
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        width: '100%',
+        height: 400,
+        borderRadius: 10,
+        overflow: 'hidden',
+    },
+    imageArea: {
+        flex: 1,
+        backgroundColor: 'grey',
+        // borderColor: 'red',
+        // borderWidth: 5,
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        overflow: 'hidden',
+    },
+    infoArea: {
+        flex: 1,
+    }
+});

9911bf16010fe5e36b0276634afd9bd30c1d570a
yeop <woco11@naver.com>
2023-11-21 20:17:44 +0900

    Fix: MenuCard 좌우 너비가 좁은 문제

--- App.js
+++ App.js
@@ -1,25 +1,27 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, FlatList, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, FlatList, SafeAreaView, Platform } from "react-native";
 
 import MenuCard from "./components/MenuCard";
 import { dishes } from "./misc/Dummy";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <View style={styles.starttitle}>
-        <Text style={styles.titleText}>오늘의 식단</Text>
-        <Text>2023년xx월xx일</Text>
-        { /* TODO: 날짜 동적으로 바꾸기 */ }
-      </View>
-      {/* <SafeAreaView>
-        <FlatList />
-      </SafeAreaView> */}
-      <View style="menuCards">
-        <MenuCard dish={dishes[0]} />
-      </View>
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* <View > */}
+        <View style={styles.starttitle}>
+          <Text style={styles.titleText}>오늘의 식단</Text>
+          <Text>2023년xx월xx일</Text>
+          { /* TODO: 날짜 동적으로 바꾸기 */ }
+        </View>
+        {/* <SafeAreaView>
+          <FlatList />
+        </SafeAreaView> */}
+        <View style={styles.menuCards}>
+          <MenuCard dish={dishes[0]} />
+        </View>
+        <StatusBar style="auto" />
+      {/* </View> */}
+    </SafeAreaView>
   );
 }
 
@@ -27,17 +29,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    paddingHorizontal: '5%',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
   },
   starttitle: {
     alignSelf: "flex-start", // 왼쪽 정렬
-    marginLeft: 10, // 좌측 여백
+    marginHorizontal: 25, // 좌측 여백
   },
   titleText: {
     fontSize: 35, //Text 폰트크기 조절
   },
   menuCards: {
-    width: '95%',
+    width: '100%',
+    flexGrow: 1,
+    alignSelf: 'center',
   }
 });
--- components/MenuCard.js
+++ components/MenuCard.js
@@ -6,6 +6,9 @@ export default function MenuCard({dish}) {
         <Shadow
             radius={10}
             offset={[0, 5]}
+            style={{
+                width: '100%'
+            }}
         >
             <View style={styles.container}>
                 <View style={styles.imageArea}>
@@ -16,7 +19,6 @@ export default function MenuCard({dish}) {
                     />
                 </View>
                 <View style={styles.infoArea}>
-                    <Text>Hello, world!</Text>
                     <Text style={styles.menuName}>{dish.name}</Text>
                     <Text style={styles.price}>{dish.price}</Text>
                 </View>
@@ -28,24 +30,40 @@ export default function MenuCard({dish}) {
 
 const styles = StyleSheet.create({
     container: {
+        // flexDirection: 'column',
         backgroundColor: 'white',
-        width: '100%',
-        height: 400,
+        // width: 'auto',
+        height: 300,
         borderRadius: 10,
-        overflow: 'hidden',
+        overflow: 'hidden', //이미지 깨지는거 방지
     },
     imageArea: {
         flex: 1,
         backgroundColor: 'grey',
+        
         // borderColor: 'red',
         // borderWidth: 5,
     },
     image: {
         width: '100%',
         height: '100%',
-        overflow: 'hidden',
+        // overflow: 'hidden',
     },
     infoArea: {
         flex: 1,
-    }
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15,
+        paddingTop: 10,
+
+
+    },
+    menuName: {
+        fontSize:28,
+       
+    },
+    price: {
+        fontSize:24,
+
+    },
 });

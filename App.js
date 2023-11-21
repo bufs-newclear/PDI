ffc87522e60675ffe23ddfd6618d0e4dbd610053
yeop <woco11@naver.com>
2023-11-21 21:06:29 +0900

    MenuCard 상세정보 템플릿 작성

--- App.js
+++ App.js
@@ -1,26 +1,47 @@
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, FlatList, Platform } from "react-native";
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Platform,
+  TouchableOpacity,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 import MenuCard from "./components/MenuCard";
 import { dishes } from "./misc/Dummy";
 
 export default function App() {
+  const [isDetailVisible, setIsDetailVisible] = useState(false);
+
+  const toggleDetailVisibility = () => {
+    setIsDetailVisible(!isDetailVisible);
+  };
   return (
     <SafeAreaView style={styles.container}>
       {/* <View > */}
-        <View style={styles.starttitle}>
-          <Text style={styles.titleText}>오늘의 식단</Text>
-          <Text>2023년xx월xx일</Text>
-          { /* TODO: 날짜 동적으로 바꾸기 */ }
-        </View>
-        {/* <SafeAreaView>
-          <FlatList />
-        </SafeAreaView> */}
-        <View style={styles.menuCards}>
-          <MenuCard dish={dishes[0]} />
-        </View>
-        <StatusBar style="auto" />
+      <View style={styles.starttitle}>
+        <Text style={styles.titleText}>오늘의 식단</Text>
+        <Text>2023년xx월xx일</Text>
+        {/* TODO: 날짜 동적으로 바꾸기 */}
+      </View>
+      <SafeAreaView>
+        <FlatList />
+        <TouchableOpacity onPress={toggleDetailVisibility}>
+          <View style={styles.menuCards}>
+            <MenuCard dish={dishes[0]} />
+            {isDetailVisible && (
+            <View style={styles.detailContainer}>
+              <Text>상세정보 내용</Text>
+              {/* 여기에 자세한내용추가 */}
+            </View>
+          )}
+          </View>
+        </TouchableOpacity>
+      </SafeAreaView>
+      <StatusBar style="auto" />
       {/* </View> */}
     </SafeAreaView>
   );
@@ -31,8 +52,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     justifyContent: "center",
-    paddingHorizontal: '5%',
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    marginHorizontal: "5%",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
   starttitle: {
     alignSelf: "flex-start", // 왼쪽 정렬
@@ -42,8 +63,14 @@ const styles = StyleSheet.create({
     fontSize: 35, //Text 폰트크기 조절
   },
   menuCards: {
-    width: '100%',
+    width: "100%",
     flexGrow: 1,
-    alignSelf: 'center',
-  }
+    alignSelf: "center",
+  },
+  detailContainer: {
+    marginTop: 10,
+    padding: 10,
+    backgroundColor: "#eee",
+    borderRadius: 5,
+  },
 });
--- components/MenuCard.js
+++ components/MenuCard.js
@@ -1,69 +1,67 @@
-import { StyleSheet, Text, View, Image }  from 'react-native';
-import { Shadow } from 'react-native-shadow-2';
+import { StyleSheet, Text, View, Image } from "react-native";
+import { Shadow } from "react-native-shadow-2";
 
-export default function MenuCard({dish}) {
-    return (
-        <Shadow
-            radius={10}
-            offset={[0, 5]}
-            style={{
-                width: '100%'
-            }}
-        >
-            <View style={styles.container}>
-                <View style={styles.imageArea}>
-                    <Image
-                        source={dish.image}
-                        style={styles.image}
-                        resizeMode='cover'
-                    />
-                </View>
-                <View style={styles.infoArea}>
-                    <Text style={styles.menuName}>{dish.name}</Text>
-                    <Text style={styles.price}>{dish.price}</Text>
-                </View>
-            </View>
-        </Shadow>
-            
-    );
-};
+export default function MenuCard({ dish }) {
+  return (
+    <Shadow
+      radius={10}
+      offset={[0, 5]}
+      style={{
+        width: "100%",
+      }}
+    >
+      <View style={styles.container}>
+        <View style={styles.imageArea}>
+          <Image source={dish.image} style={styles.image} resizeMode="cover" />
+        </View>
+        <View style={styles.infoArea}>
+          <Text style={styles.menuName}>{dish.name}</Text>
+          <Text style={styles.price}>{dish.price}</Text>
+          <View>
+            <Text style={styles.detailText}>상세정보</Text>
+          </View>
+        </View>
+      </View>
+    </Shadow>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        // flexDirection: 'column',
-        backgroundColor: 'white',
-        // width: 'auto',
-        height: 300,
-        borderRadius: 10,
-        overflow: 'hidden', //이미지 깨지는거 방지
-    },
-    imageArea: {
-        flex: 1,
-        backgroundColor: 'grey',
-        
-        // borderColor: 'red',
-        // borderWidth: 5,
-    },
-    image: {
-        width: '100%',
-        height: '100%',
-        // overflow: 'hidden',
-    },
-    infoArea: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingHorizontal: 15,
-        paddingTop: 10,
+  container: {
+    // flexDirection: 'column',
+    backgroundColor: "white",
+    // width: 'auto',
+    height: 300,
+    borderRadius: 10,
+    overflow: "hidden", //이미지 깨지는거 방지
+  },
+  imageArea: {
+    flex: 1,
+    backgroundColor: "grey",
 
-
-    },
-    menuName: {
-        fontSize:28,
-       
-    },
-    price: {
-        fontSize:24,
-
-    },
+    // borderColor: 'red',
+    // borderWidth: 5,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    // overflow: 'hidden',
+  },
+  infoArea: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+    paddingTop: 10,
+  },
+  menuName: {
+    fontSize: 28,
+  },
+  price: {
+    fontSize: 24,
+  },
+  detailText: {
+    color: "blue",
+    marginTop: "auto",
+  },
 });

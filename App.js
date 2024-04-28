f117a82450369d4504082bc61d5e869e1d9b5dea
yeop <woco11@naver.com>
2024-04-28 14:35:15 +0900

    위젯 초기화 및 의존성 업데이트
    Co-authored-by: Ryu jongheon <me@lapy.link>

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Text } from 'react-native';
+import { Text, View, ActivityIndicator } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { StatusBar, Button } from "expo-status-bar";
+import { StatusBar } from "expo-status-bar";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Constants from 'expo-constants';
 import { FontAwesome5 } from "@expo/vector-icons";
 import Meals from "./pages/Meals";
 import Ranking from "./pages/Ranking";
+import { HelloWidgetPreviewScreen } from "./widget/android_widget"; // Import the HelloWidgetPreviewScreen
 
 const Tab = createBottomTabNavigator();
 
@@ -39,38 +40,42 @@ const App = () => {
     setIsLoggedIn(false);
   };
 
+  // if (loading) {
+  //   return <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><ActivityIndicator size="large" /></View>;
+  // }
+
   return (
     <>
-      <StatusBar />
-      {loading ? (
-        <Text>Loading...</Text>
-      ) : (
-        <NavigationContainer>
-          <Tab.Navigator
-            screenOptions={({ route }) => ({
-              tabBarActiveTintColor: "orange",
-              tabBarIcon: ({ focused, color, size }) => {
-                let iconName = 'archive';
-                if (route.name === "메뉴") {
-                  iconName = 'utensils';
-                } else if (route.name === "역대 랭킹") {
-                  iconName = 'trophy';
-                }
-                return <FontAwesome5 name={iconName} size={size} color={color} />
-              },
-            })}
-          >
-            <Tab.Screen name="메뉴"
-              component={Meals}
-              options={{ headerShown: false }}
-            />
-            <Tab.Screen name="역대 랭킹"
-              component={Ranking}
-              options={{ headerShown: false }}
-            />
-          </Tab.Navigator>
-        </NavigationContainer>
-      )}
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarActiveTintColor: "orange",
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName = 'archive';
+              if (route.name === "메뉴") {
+                iconName = 'utensils';
+              } else if (route.name === "역대 랭킹") {
+                iconName = 'trophy';
+              }
+              return <FontAwesome5 name={iconName} size={size} color={color} />
+            },
+          })}
+        >
+          <Tab.Screen name="메뉴"
+            component={Meals}
+            options={{ headerShown: false }}
+          />
+          <Tab.Screen name="역대 랭킹"
+            component={Ranking}
+            options={{ headerShown: false }}
+          />
+          <Tab.Screen name="위젯 미리보기"
+            component={HelloWidgetPreviewScreen}
+            options={{ headerShown: false }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
     </>
   );
 };
